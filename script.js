943e2bb9775f3a66cb34e1f3f38e338a0ae3ceba
Mohamed Amine AKAABOUNE <akaaboune.mohamedamine@gmail.com>
2018-07-09 01:50:12 +0100

    Adding Project Files

---
+++ script.js
@@ -0,0 +1,22 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+var cssValue = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+console.log(color1 + color2);
+function setGradient() {
+	body.style.background =
+	"linear-gradient(to right, "
+	+ color1.value
+	+ ", "
+	+ color2.value
+	+ ")";
+
+	cssValue.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+});
